97430722a7c65fcd998053a567f623c8a073c3ad
Khaleel Lee <kbasha968@gmail.com>
2021-02-13 19:34:42 +0530

    Editing & Change Content

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/contact', function (req, res, next) {
+	res.render('contact', { title: 'Express' });
+});
+
+router.get('/about', function (req, res, next) {
+	res.render('about', { title: 'Express' });
+});
+
+router.get('/services', function (req, res, next) {
+	res.render('services', { title: 'Express' });
+});
+
+router.post('/contactus', async (req, res, next) => {
+	console.log('route called');
+	let fname = req.body.fname;
+	let lname = req.body.lname;
+	let email = req.body.email;
+	let message = req.body.message;
+
+	// let response = await sendEmail({
+	// 	email: 'sateeshreddy.y@byovet.com',
+	// 	subject: `${(fname, lname)} with email : ${email} has contacted Byovet`,
+	// 	text: message,
+	// });
+	if (response.status === 200) {
+		res.status(200).send({ msg: 'OK' });
+	} else {
+		console.log('could not send email');
+		// res.status(200).send({ msg: 'OK' });
+	}
 });
 
 module.exports = router;
